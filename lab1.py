9a48c3619059152a0b6fa56112d55f04ed425270
malikovmarlen <82810726+malikovmarlen@users.noreply.github.com>
2023-03-09 13:34:13 +0600

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,131 @@
+# EX1 
+print('Hello World')
+
+# EX2 
+if 5 > 2:
+  print("Five is greater than two!")
+
+# EX3 
+#This is a comment
+
+# EX4
+"""
+This is a comment
+written in 
+more than just one line
+"""
+
+# EX5
+carname = "Volvo"
+
+# EX6
+x = 50
+
+# EX7
+x = 5
+y = 10
+print(x + y)
+
+# EX8
+x = 5
+y = 10
+z = x + y
+print(z)
+
+# EX9
+myfirst_name = "John"
+
+# EX10
+x = y = z = "Orange"
+
+# EX11
+def myfunc():
+  global x 
+  x = "fanstatic"
+
+# EX12
+x = 5
+print(type(x))
+
+int
+
+# EX13
+x = 'Hello World'
+print(type(x))
+
+str
+
+# EX14
+x = 20.5
+print(type(x))
+
+float
+
+# EX15
+x = ['apple', 'banana', 'orange']
+print(type(x))
+
+list
+
+# EX16
+x = ('apple', 'banana', 'orange')
+print(type(x))
+
+tuple
+
+# EX17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+dict
+
+# EX18
+x = True
+print(type(x))
+
+bool
+
+# EX19
+x = 5
+x = float(x)
+
+# EX20
+x = 5.5
+x = int(x)
+
+# EX21
+x = 5
+x = complex(x)
+
+# EX22
+x = "Hello World"
+print(len(x))
+
+# EX23
+txt = "Hello World"
+x = txt[0]
+
+# EX24
+txt = "Hello World"
+x = txt[2:5]
+
+# EX25
+txt = " Hello World "
+x = txt.strip()
+
+# EX26
+txt = "Hello World"
+txt = txt.upper()
+
+# EX27
+txt = "Hello World"
+txt = txt.lower()
+
+# EX28
+txt = "Hello World"
+txt = txt.replace('H', 'J')
+
+# EX29
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
